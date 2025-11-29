3a14eadbc4da701b640f54bdabb7a33b1682103e
Randall Lantigua <114352598+LantiguaRandall@users.noreply.github.com>
2025-11-29 14:56:23 +0000

    eslint cchange

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -99,7 +99,7 @@ export default defineConfig([
       "react-hooks/exhaustive-deps": "off",
       "jsx-a11y/click-events-have-key-events": "warn",
       "jsx-a11y/interactive-supports-focus": "warn",
-      "prettier/prettier": "warn",
+      "prettier/prettier": "off", // Changed from "warn" to "off"
       "no-unused-vars": "off",
       "unused-imports/no-unused-vars": "off",
       "unused-imports/no-unused-imports": "warn",
