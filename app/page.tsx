679636e97227a402af200491d44be9d6b11ae8ab
Randall Lantigua <114352598+LantiguaRandall@users.noreply.github.com>
2025-11-29 15:13:19 +0000

    next update

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { Hero } from "@/components/hero";
 import { Features } from "@/components/features";
 import { About } from "@/components/about";
+import { Testimonial } from "@/components/testimonial";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Features />
       <About />
+      <Testimonial />
     </div>
   );
 }
